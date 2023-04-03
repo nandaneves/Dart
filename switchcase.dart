c84f8f320f66e34c87e6115f1d22c7db23d2b85b
FERNANDA MIRANDA NEVES <fernanda.2960@aluno.pr.senac.br>
2023-04-03 11:55:05 -0300

    oii dieimes

---
+++ switchcase.dart
@@ -0,0 +1,13 @@
+void main() {
+  String linguagem = "HTML";
+  switch (linguagem) {
+    case "HTML":
+      print("linguagem foda");
+      break;
+    case "PHP":
+      print("linguagem");
+      break;
+    case "JavaScript":
+      print("linguagem");
+  }
+}
